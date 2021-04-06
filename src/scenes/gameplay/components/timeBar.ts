0a94183c8b714ca59b216eda3efb0d5a63f8616b
Natchapol Srisang <utopiabeam@gmail.com>
2021-04-06 09:17:45 +0700

    fix: fix typo

--- src/scenes/gameplay/components/timeBar.ts
+++ src/scenes/gameplay/components/timeBar.ts
@@ -58,14 +58,14 @@ export const loadTimeBar = (resources: PIXI.IResourceDictionary) => {
 
   // init value
   timeBar.outerTimeBar.width =
-    TIME_BAR_CONFIG.TIMR_PER_TURN * (timeBar.barWidth / TIME_BAR_CONFIG.TIMR_PER_TURN)
-  timeBar.timeLeft.text = TIME_BAR_CONFIG.TIMR_PER_TURN.toString()
+    TIME_BAR_CONFIG.TIME_PER_TURN * (timeBar.barWidth / TIME_BAR_CONFIG.TIME_PER_TURN)
+  timeBar.timeLeft.text = TIME_BAR_CONFIG.TIME_PER_TURN.toString()
 
   return timeBar
 }
 
 const setTime = (timeBar: TimeBarType, timeLeft: number) => {
-  timeBar.outerTimeBar.width = timeLeft * (timeBar.barWidth / TIME_BAR_CONFIG.TIMR_PER_TURN)
+  timeBar.outerTimeBar.width = timeLeft * (timeBar.barWidth / TIME_BAR_CONFIG.TIME_PER_TURN)
   timeBar.timeLeft.text = timeLeft.toString()
 }
 
